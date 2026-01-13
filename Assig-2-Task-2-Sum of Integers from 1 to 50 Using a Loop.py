dc0617a8ea735a8394dfef854a0948710099605c
SumitSrivastava24x7 <sumit.srivastava56@gmail.com>
2026-01-13 14:49:56 +0530

    Add files via upload

---
+++ Assig-2-Task-2-Sum of Integers from 1 to 50 Using a Loop.py
@@ -0,0 +1,13 @@
+__author__ = "Sumit Srivastava"
+__copyright__ = "Sumit Srivastava"
+__email__ = "sumit.srivastava56@gmail.com"
+__date__ = "01-13-2026 [DD-MM-YYYY]"
+"""
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range.
+"""
+i = 0
+for k in range(1,50):
+    i += k
+print("The sum of numbers from 1 to 50 is:",i)
+
---
+++ Assig-2-Task1-Check if a Number is Even or Odd.py
@@ -0,0 +1,13 @@
+__author__ = "Sumit Srivastava"
+__copyright__ = "Sumit Srivastava"
+__email__ = "sumit.srivastava56@gmail.com"
+__date__ = "01-13-2026 [DD-MM-YYYY]"
+"""
+1. 	Takes an integer input from the user.
+2. 	Checks whether the number is even or odd using an if-else statement.
+"""
+varnum = int(input("Enter a number: "))
+if varnum % 2 == 0:
+    print(varnum,"is a even number.")
+else:
+    print(varnum, "is as odd number.")
